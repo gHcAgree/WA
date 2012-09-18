15419bd42670f67d10e515adfb80d4797bf1880b
gHcAgree <guiggle90@gmail.com>
2012-09-18 18:54:16 +0800

    version1.0

--- a2/Main3.java
+++ a2/Main3.java
@@ -5,13 +5,13 @@ import java.util.*;
 
 public class Main3 {
     public static void main(String[] args) {
-	int a[] = {2,1,2,6,4,6,2};
+	int a[] = {7,1,3,3,5,1,2,4,3};
         ExamPeekableQueueImpl<Integer> queue = new ExamPeekableQueueImpl<Integer>(); 
 	for(int i=0;i<a.length;i++)
 	    queue.enqueue(a[i]);
 	
-        for(int i=0;i<5;i++)
-	    queue.dequeue();
+        //for(int i=0;i<5;i++)
+	    //queue.dequeue();
 
 	int max = queue.peekMaximum();
 	int min = queue.peekMinimum();
